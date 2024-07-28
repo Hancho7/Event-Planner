79955aaf63dc03eb86f28866b32d312ed2b9dd64
Hancho7 <litorojose1@gmail.com>
2024-07-28 10:50:10 +0000

    mod

--- frontend/src/pages/aboutUs.jsx
+++ frontend/src/pages/aboutUs.jsx
@@ -10,9 +10,24 @@ import "aos/dist/aos.css";
 
 function AboutUsPage() {
   const team = [
-    { name: "Osei-Bonsu Elton", role: "CEO", image: team1, lowResImage: aboutusLowRes },
-    { name: "Abdul-Aziz Nasiru", role: "Manager", image: team2, lowResImage: aboutusLowRes },
-    { name: "Frank Naro", role: "Director", image: team3, lowResImage: aboutusLowRes },
+    {
+      name: "Mathias Lawson Prince",
+      role: "Frontend-dev",
+      image: team2,
+      lowResImage: aboutusLowRes,
+    },
+    {
+      name: "Jose Jefferson",
+      role: "Backend-dev",
+      image: team1,
+      lowResImage: aboutusLowRes,
+    },
+    {
+      name: "Nana Aduseh Poku",
+      role: "Data Scientist",
+      image: team3,
+      lowResImage: aboutusLowRes,
+    },
   ];
 
   const [aboutImageLoaded, setAboutImageLoaded] = useState(false);
@@ -30,7 +45,9 @@ function AboutUsPage() {
       <div className="h-[70vh] flex items-center justify-center relative">
         <img
           loading="lazy"
-          className={`object-cover h-full w-full transition-opacity duration-700 ${aboutImageLoaded ? 'opacity-100' : 'opacity-0'}`}
+          className={`object-cover h-full w-full transition-opacity duration-700 ${
+            aboutImageLoaded ? "opacity-100" : "opacity-0"
+          }`}
           src={aboutus}
           alt="About Us"
           onLoad={handleAboutImageLoad}
@@ -51,15 +68,18 @@ function AboutUsPage() {
       </div>
 
       <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 flex flex-col items-center">
-        <h4 className="text-xl md:text-2xl font-semibold">Know More About Us</h4>
+        <h4 className="text-xl md:text-2xl font-semibold">
+          Know More About Us
+        </h4>
         <div className="max-w-4xl mx-auto">
           <p className="mt-4 md:text-lg">
-            At <span className="text-black">Event</span><span className="text-[#ffdd50]">Center</span>, we specialize in transforming events into
-            unforgettable experiences. Our mission is to provide seamless,
-            innovative, and personalized event management solutions that exceed
-            our clients' expectations. With a strong commitment to integrity,
-            excellence, and customer focus, we ensure that every event we manage
-            is executed to perfection.
+            At <span className="text-black">Event</span>
+            <span className="text-[#ffdd50]">Center</span>, we specialize in
+            transforming events into unforgettable experiences. Our mission is
+            to provide seamless, innovative, and personalized event management
+            solutions that exceed our clients' expectations. With a strong
+            commitment to integrity, excellence, and customer focus, we ensure
+            that every event we manage is executed to perfection.
           </p>
           <p className="mt-4 md:text-lg">
             Our mission is to provide high-quality services and products to our
@@ -76,9 +96,11 @@ function AboutUsPage() {
             We are passionate about pushing the boundaries of creativity and
             innovation in event management. Whether you are a client, a partner,
             or a team member, we invite you to join us on this exciting journey
-            and be a part of our story. At <span className="text-black">Event</span><span className="text-[#ffdd50]">Center</span>, the future of
-            event management is bright, and we look forward to creating
-            extraordinary events together.
+            and be a part of our story. At{" "}
+            <span className="text-black">Event</span>
+            <span className="text-[#ffdd50]">Center</span>, the future of event
+            management is bright, and we look forward to creating extraordinary
+            events together.
           </p>
           <h2 className="mt-8 text-2xl text-center font-bold text-black-700">
             Here are our team members
